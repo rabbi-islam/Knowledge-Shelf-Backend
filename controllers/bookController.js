215550202310fbad9d0c0f88a8ecccd9b0c4f304
Rabbi <rabbyislam7777@gmail.com>
2024-09-25 11:06:38 +0600

    all book api complete

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -4,34 +4,87 @@ const { throwError } = require("../utils/throwError")
 const { uploadImageHandler } = require("../utils/uploadImage")
 
 
-const addBook = async (req, res)=>{
+const addBook = async (req, res) => {
 
     console.log(req.file)
 
-    const{name,authorName} = req.body
+    const { name, authorName } = req.body
 
     const existingBook = await bookModel.findOne({
         name: { $regex: new RegExp(`^${name}$`, 'i') },
-        authorName: { $regex: new RegExp(`^${authorName}$`, 'i') } 
+        authorName: { $regex: new RegExp(`^${authorName}$`, 'i') }
     })
 
-    if(existingBook) throwError("Book already Exist!",400)
+    if (existingBook) throwError("Book already Exist!", 400)
 
-    const bookImage = await uploadImageHandler(req.file,"Knowledge_Shelf")
+    const bookImage = await uploadImageHandler(req.file, "Knowledge_Shelf")
 
     const newBook = await bookModel.create({
         ...req.body,
         image: bookImage.secure_url,
         imageKey: bookImage.public_id,
 
-   })
+    })
 
     res.status(201).json({
-		success: true,
-		book: newBook,
-	});
-
+        success: true,
+        book: newBook,
+    });
 
 }
 
-module.exports = {addBook}
+const getAllBook = async (req, res) => {
+
+    const allBook = await bookModel.find();
+
+    if (allBook.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No Books List Available."
+      });
+    }
+    
+    res.status(200).json({
+      success: true,
+      books: allBook
+    });
+    
+}
+
+const getSingleBook = async (req, res) => {
+    const book = await bookModel.findOne({ _id: req.params.id });
+
+    if (!book) {
+      return res.status(404).json({
+        success: false,
+        message: "No Book Found with the given ID."
+      });
+    }
+    
+    res.status(200).json({
+      success: true,
+      book: book
+    });
+}
+
+const deleteBook = async (req, res) => {
+
+
+    const result = await bookModel.deleteOne({ _id: req.params.id });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No book found with the given ID"
+      });
+    }
+    
+    res.status(200).json({
+      success: true,
+      message: "Book deleted successfully"
+    });
+    
+    
+}
+
+module.exports = { addBook, getAllBook, deleteBook, getSingleBook }
--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,9 +1,12 @@
 const express = require("express")
-const { addBook } = require("../controllers/bookController")
+const { addBook, getAllBook, deleteBook, getSingleBook } = require("../controllers/bookController")
 const upload = require("../middleware/upload")
 const catchAsync = require("../utils/catchError")
 const router = express.Router() 
 
 router.post("/book",upload.single("image") ,catchAsync(addBook))
+router.get("/book", catchAsync(getAllBook))
+router.get("/book/:id", catchAsync(getSingleBook))
+router.delete("/book/:id", catchAsync(deleteBook))
 
 module.exports = router
