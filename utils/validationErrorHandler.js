c957af41393be28bd8e717715b9dde6e35d61efc
Rabbi <rabbyislam7777@gmail.com>
2024-09-28 11:15:06 +0600

    fix server error

--- utils/validationErrorHandler.js
+++ utils/validationErrorHandler.js
@@ -1,7 +1,7 @@
 // Helper function to handle validation errors
 
 const { validationResult } = require("express-validator");
-const handleValidationErrors = (req, res) => {
+const handleValidationErrors = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ success: false, message: errors.array().map(((msg)=>msg.msg)) });
